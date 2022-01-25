7df78c4d78d759a5684966a7482bbfa2ba1799ae
habtamu alemayehu <habtamu.alemayehu@wku.edu.et>
2022-01-25 22:49:16 +0300

    Java RMI-Chat

---
+++ src/ChatClientInt.java
@@ -0,0 +1,8 @@
+
+
+import java.rmi.*;
+
+public interface ChatClientInt extends Remote{	
+	public void tell (String name)throws RemoteException ;
+	public String getName()throws RemoteException ;
+}
---
+++ src/ChatServerInt.java
@@ -0,0 +1,10 @@
+
+
+import java.rmi.*;
+import java.util.*;
+
+public interface ChatServerInt extends Remote{	
+	public boolean login (ChatClientInt a)throws RemoteException ;
+	public void publish (String s)throws RemoteException ;
+	public Vector getConnected() throws RemoteException ;
+}
